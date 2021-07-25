5611a7e94c67527ddafbb4127c8ad26b3bb8aa30
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-07-25 12:39:23 +0300

    Make a private route for the phone book

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import AppBar from './AppBar';
 import HomeView from '../views/HomeView';
 import LoginView from '../views/LoginView';
 import ContactsView from '../views/ContactsView';
+import PrivatRoute from './PrivatRoute';
 
 class App extends Component {
   componentDidMount() {
@@ -17,15 +18,13 @@ class App extends Component {
   render() {
     return (
       <Container>
-        
-          <AppBar />
-          <Switch>
-            <Route exact path="/" component={HomeView} />
-            <Route path="/register" component={RegisterView} />
-            <Route path="/login" component={LoginView} />
-            <Route path="/contacts" component={ContactsView} />
-          </Switch>
-        
+        <AppBar />
+        <Switch>
+          <Route exact path="/" component={HomeView} />
+          <Route path="/register" component={RegisterView} />
+          <Route path="/login" component={LoginView} />
+          <PrivatRoute path="/contacts" component={ContactsView} />
+        </Switch>
       </Container>
     );
   }
---
+++ src/components/PrivatRoute.js
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+import { getIsAuthenticated } from '../redux/auth';
+
+const PrivateRoute = ({
+  component: Component,
+  isAuthenticated,
+  ...routeProps
+}) => (
+  <Route
+    {...routeProps}
+    render={props =>
+      isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
+    }
+  />
+);
+
+const mapStateToProps = state => ({
+  isAuthenticated: getIsAuthenticated(state),
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
